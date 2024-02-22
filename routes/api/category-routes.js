3825ce22cd34e24089b0da75961b21c112b4cff9
sailorshy94 <sbutle94@gmail.com>
2024-02-21 23:00:27 -0500

    working in category-routes - created the get method to find all categories and include associated products, started to map out get one category by id, started mapping out create new category, created method to update category based on id val, created method to delete category by id val

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,28 +4,58 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // TODO: find all categories
+  // find all categories
   // be sure to include its associated Products
-  constCategData = await Category.findAll({ include: [{ 
-    model: Product
-  }]})
+  const categData = await Category.findAll({
+    include: [{
+      model: Product,
+      attributes: ['product_name']
+    }]
+  });
+  return res.json(categData);
 });
 
 router.get('/:id', async (req, res) => {
   // TODO: find one category by its `id` value
   // be sure to include its associated Products
+  const categData = await Category.findByPk(req.params.id);
+  // not sure about the include part - where should it go?
+  return res.json(categData);
+  // ({
+  // include: [{
+  //   model: Product,
+  //   attributes: ['product_name']
+  // }]
 });
+
 
 router.post('/', async (req, res) => {
   // TODO: create a new category
+  const categData = await ({});
 });
 
 router.put('/:id', async (req, res) => {
-  // TODO: update a category by its `id` value
+  // update a category by its `id` value
+  const categData = await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+  return res.json(categData);
 });
 
 router.delete('/:id', async (req, res) => {
-  // TODO: delete a category by its `id` value
+  // delete a category by its `id` value
+  const categData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  return res.json(categData);
 });
 
 module.exports = router;
