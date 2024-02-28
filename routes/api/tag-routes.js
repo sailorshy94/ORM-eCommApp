c35a7e7e62aade12c5129c0bb56fb92417eb1403
sailorshy94 <sbutle94@gmail.com>
2024-02-27 20:59:53 -0500

    added code for routes to create new tag, update tag by id val, find all and find single tag by id; also added try...catch to router.post method in tag-routes, will add to all

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,48 +6,53 @@ const { Tag, Product, ProductTag } = require('../../models');
 // TODO: add try...catch to each crud
 
 router.get('/', async (req, res) => {
-  // TODO: find all tags
+  // find all tags
   // be sure to include its associated Product data
-  const tagData = await Tag.findAll({
-    // {
-    //   model: Product,
-    //   through: ProductTag,
-    //   attributes: ['product_name']
-    // }]
-
-  });
-  return
+  const tagData = await Tag.findAll(
+    {
+      model: Product,
+      through: ProductTag,
+      attributes: ['product_name']
+    })
+  return res.json(tagData);
 });
 
+
 router.get('/:id', async (req, res) => {
-  // TODO: find a single tag by its `id`
+  // find a single tag by its `id`
   // be sure to include its associated Product data
-  const tag = await Tag.findByPk({
-// {
-    //   model: Product,
-    //   through: ProductTag,
-    //   attributes: ['product_name']
-    // }]
-  });
-  return res.json();
+  const tag = await Tag.findByPk(req.params.id,
+    {
+      model: Product,
+      through: ProductTag,
+      attributes: ['product_name']
+    });
+  return res.json(tag);
 });
 
 router.post('/', async (req, res) => {
-  // TODO: create a new tag
-  const tag = await Tag.create(req.body);
-  // return res.json();
+  // create a new tag
+  try {
+    const tag = await Tag.create(req.body);
+    return res.json(200).json(tag);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // TODO: update a tag's name by its `id` value
-  const tagData = await Tag.update({
-  },
-  {
-    where: {
-
-    }
-  });
-  return res.json();
+  // update a tag's name by its `id` value
+  const tag = await Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      }
+    });
+  return res.json(tag);
 });
 
 router.delete('/:id', async (req, res) => {
